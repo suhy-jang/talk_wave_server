8428a3a9a968bbbdd853e6028bbaae73400f8ec5
Suhyeon Jang <suhy.jang.shj@gmail.com>
2023-08-03 12:34:27 +0900

    Start express server with socket

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+io.on('connection', (socket) => {
+  console.log('User connected');
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
