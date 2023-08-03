bd3fa5850edc4c0a86009e6c11e3c4385bc61e9c
Suhyeon Jang <suhy.jang.shj@gmail.com>
2023-08-03 22:40:04 +0900

    Set cors origin

--- index.js
+++ index.js
@@ -4,15 +4,27 @@ const socketIo = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
+const io = socketIo(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+    credentials: true,
+  },
+});
 
 io.on('connection', (socket) => {
   console.log('User connected');
+
+  socket.on('sendMessage', (message) => {
+    console.log(`send message: ${message}`);
+    io.emit('receiveMessage', message);
+  });
+
   socket.on('disconnect', () => {
     console.log('User disconnected');
   });
 });
 
-server.listen(3000, () => {
-  console.log('Listening on port 3000');
+server.listen(4000, () => {
+  console.log('Listening on port 4000');
 });
