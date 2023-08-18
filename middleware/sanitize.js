a4fdc669b973b902c7b46765fd00797b61e3248e
Suhyeon Jang <suhy.jang.shj@gmail.com>
2023-08-18 10:38:30 +0900

    Remove comments

--- middleware/sanitize.js
+++ middleware/sanitize.js
@@ -16,15 +16,6 @@ const sanitize = (fieldsToSanitize) => (req, res, next) => {
   req.body = { ...req.body, ...sanitizedBody };
 
   next();
-
-  // TODO: sanitize
-
-  // logger.error(errors);
-  // return res.status(400).json({
-  //   errors: errors.array().map(({ path, value, msg }) => ({
-  //     msg: `'${path}' with value '${value}' has '${msg}' error`,
-  //   })),
-  // });
 };
 
 module.exports = sanitize;
